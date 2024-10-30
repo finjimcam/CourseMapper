4854d40495feae4069506d11fc5ad2f84c2b62db
Maxine <maxine.armstrong2005@gmail.com>
2024-10-30 18:40:40 +0000

    strict type checking enabled, forcing us to use types. This prevents runtime type errors.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Any
 
 from fastapi import FastAPI
 
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, Any]:
     return {"item_id": item_id, "q": q}
