df72338e243c35e0934c37ef2f3bd5aae25b639c
phantomsheepp <rachel.horton1@icloud.com>
2024-11-13 14:30:20 +0000

    installed axios package and added axios API call

---
+++ frontend/src/pages/Workbook.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "axios";
+
+function Workbook() {
+    let { workbook_id } = useParams();
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/workbook/${workbook_id}/`)
+        .then(response => {
+            console.log(response)
+        }) 
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
+
+    return <></>
+}
+
+export default Workbook
