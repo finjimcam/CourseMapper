1bf2b3ce729be0f4792007d7977ce6fb456d22c4
phantomsheepp <rachel.horton1@icloud.com>
2024-11-13 15:16:48 +0000

    moved brackets so working queries for workbook week view

--- backend/main.py
+++ backend/main.py
@@ -36,14 +36,14 @@ def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
 
-# Endpoint to fetch all Users
+# Fetch all Users
 @app.get("/users/")
 def read_users(session: Session = Depends(get_session)) -> List[User]:
     users = list(session.exec(select(User)).all())
     return users
 
 
-# Endpoint to fetch all PermissionsGroups
+# Fetch all PermissionsGroups
 @app.get("/permissions-groups/")
 def read_permissions_groups(
     session: Session = Depends(get_session),
@@ -52,14 +52,14 @@ def read_permissions_groups(
     return permissions_groups
 
 
-# Endpoint to fetch all Courses
+# Fetch all Courses
 @app.get("/courses/")
 def read_courses(session: Session = Depends(get_session)) -> List[Course]:
     courses = list(session.exec(select(Course)).all())
     return courses
 
 
-# Endpoint to fetch all LearningPlatforms
+# Fetch all LearningPlatforms
 @app.get("/learning-platforms/")
 def read_learning_platforms(
     session: Session = Depends(get_session),
@@ -68,7 +68,7 @@ def read_learning_platforms(
     return learning_platforms
 
 
-# Endpoint to fetch all LearningActivities
+# Fetch all LearningActivities
 @app.get("/learning-activities/")
 def read_learning_activities(
     session: Session = Depends(get_session),
@@ -77,30 +77,37 @@ def read_learning_activities(
     return learning_activities
 
 
-# Endpoint to fetch all TaskStatuses
+# Fetch all TaskStatuses
 @app.get("/task-statuses/")
 def read_task_statuses(session: Session = Depends(get_session)) -> List[TaskStatus]:
     task_statuses = list(session.exec(select(TaskStatus)).all())
     return task_statuses
 
 
-# Endpoint to fetch all LearningTypes
+# Fetch all LearningTypes
 @app.get("/learning-types/")
 def read_learning_types(session: Session = Depends(get_session)) -> List[LearningType]:
     learning_types = list(session.exec(select(LearningType)).all())
     return learning_types
 
 
+# Fetches all workbooks
+@app.get("/workbook/all/")
+def read_workbooks(session: Session = Depends(get_session)) -> List[Workbook]:
+    workbooks = list(session.exec(select(Workbook)).all())
+    return workbooks
+
+
 # Fetches a specified workbook
 @app.get("/workbook/{workbook_id}/")
-def read_workbook_week(workbook_id: int, session: Session = Depends(get_session)) -> List[Activity]:
-    workbook = list(session.exec(select(Workbook)).where(Workbook.workbook_id==workbook_id))[0] 
+def read_specified_workbook(workbook_id: int, session: Session = Depends(get_session)) -> Workbook:
+    workbook = list(session.exec(select(Workbook).where(Workbook.id==workbook_id)))[0] 
     return workbook
 
 
-# Fetches activities for a specified workbook week
+# Fetches activities for a specified workbook weekst[
 @app.get("/activity/{workbook_id}/{week_number}/")
 def read_workbook_week(workbook_id: int, week_number: int, 
                        session: Session = Depends(get_session)) -> List[Activity]:
-    activities = list(session.exec(select(Activity)).where(Activity.workbook_id==workbook_id and Activity.week_number==week_number))
+    activities = list(session.exec(select(Activity).where(Activity.workbook_id==workbook_id and Activity.week_number==week_number)))
     return activities
