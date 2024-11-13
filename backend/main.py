b730538c0ca214d49a7d9a10a4f10cf0f2de20c9
phantomsheepp <rachel.horton1@icloud.com>
2024-11-13 00:52:56 +0000

    added parameterised workbook views

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,8 @@ from backend.models.models import (
     LearningActivity,
     TaskStatus,
     LearningType,
+    Activity,
+    Workbook
 )
 
 SessionDep = Annotated[Session, Depends(get_session)]
@@ -28,17 +30,10 @@ async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
 
 app = FastAPI(lifespan=lifespan)
 
-
+# TODO: delete this
 @app.get("/")
 def read_root() -> dict[str, str]:
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(
-    item_id: int, q: Union[str, None] = None
-) -> dict[str, Union[int, str, None]]:
-    return {"item_id": item_id, "q": q}
 
 
 # Endpoint to fetch all Users
@@ -94,3 +89,18 @@ def read_task_statuses(session: Session = Depends(get_session)) -> List[TaskStat
 def read_learning_types(session: Session = Depends(get_session)) -> List[LearningType]:
     learning_types = list(session.exec(select(LearningType)).all())
     return learning_types
+
+
+# Fetches a specified workbook
+@app.get("/workbook/{workbook_id}/")
+def read_workbook_week(workbook_id: int, session: Session = Depends(get_session)) -> List[Activity]:
+    workbook = list(session.exec(select(Workbook)).where(Workbook.workbook_id==workbook_id))[0] 
+    return workbook
+
+
+# Fetches activities for a specified workbook week
+@app.get("/activity/{workbook_id}/{week_number}/")
+def read_workbook_week(workbook_id: int, week_number: int, 
+                       session: Session = Depends(get_session)) -> List[Activity]:
+    activities = list(session.exec(select(Activity)).where(Activity.workbook_id==workbook_id and Activity.week_number==week_number))
+    return activities
