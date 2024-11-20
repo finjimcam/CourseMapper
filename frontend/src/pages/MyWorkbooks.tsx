c8fb6803f417e2c0f9b37a882769ed2d7863edf5
phantomsheepp <rachel.horton1@icloud.com>
2024-11-20 14:24:02 +0000

    base My Workbooks incl search and carousel

--- frontend/src/pages/MyWorkbooks.tsx
+++ frontend/src/pages/MyWorkbooks.tsx
@@ -1,12 +1,18 @@
+import SearchBar from "../components/Searchbar";
+import Carousel from "../components/Carousel";
+
 function MyWorkbooks() {
+    let workbooks = [
+        {id:1, content:"Workbook 1"},
+        {id:2, content:"Workbook 2"}
+    ]
+
     return (
         <>
-        <h1>TESTING MY WORKBOOKS PAGE</h1>
+        <h1>My Workbooks</h1>
 
-        <h2>PLS BE IN MIDDLE</h2>
-
-        <h3>YOU CAN SWITCH BACK AND FORTH BETWEEN HOME AND ABOUT</h3>
-
+        <SearchBar/>
+        <Carousel items={workbooks}/>
         </>
     );
 }
