2978654c673422994d371c04c983e32d4776847f
Qikai Zhou <2616452q@student.gla.ac.uk>
2024-11-20 16:15:18 +0000

    Changed the absolute path of the database in the database file to a relative path so that fastAPI can run in backend

--- backend/models/database.py
+++ backend/models/database.py
@@ -1,9 +1,14 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from typing import Annotated, Iterator
 from fastapi import Depends
 from sqlmodel import Session, SQLModel, create_engine
 
 
-sqlite_file_name = "backend/database.db"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sqlite_file_name = os.path.join(current_dir, "database.db")
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
