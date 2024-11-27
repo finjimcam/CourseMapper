459630ab100a7a99c6fdb71591dc2cdd343ca462
Qikai Zhou <2616452q@student.gla.ac.uk>
2024-11-27 11:29:08 +0000

    just a test

--- backend/models/database.py
+++ backend/models/database.py
@@ -6,7 +6,7 @@ from sqlmodel import Session, SQLModel, create_engine
 sqlite_file_name = "backend/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread": False}
+connect_args = {"check_same_thread": False }
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
