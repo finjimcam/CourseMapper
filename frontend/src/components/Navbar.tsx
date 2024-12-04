c72e66e7f716d5f03980d6f5350d5f8f7ab4a44d
Ibrahim Asghar <2617847A@student.gla.ac.uk>
2024-12-04 09:51:01 +0000

    Made headings blue when page is active

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -93,7 +93,7 @@ function Navbar() {
                 </div>
                 <div
                     className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-                    id="navbar-user"
+                    id="navbar-user"    
                 >
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
                         <li>
