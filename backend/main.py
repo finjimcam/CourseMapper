02af6b80d2c1913d18ad9e28c777e6e0b4c4836e
Qikai Zhou <2616452q@student.gla.ac.uk>
2025-01-17 10:16:51 +0000

    fix the different module names problem

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from typing import Union, Annotated, AsyncGenerator, List, Dict, Any
+from typing import Annotated, AsyncGenerator, List, Dict, Any
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import Session, select
