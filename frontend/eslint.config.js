11a5b7d8033ddf49185fab24df427d6e4c76bc19
Rachel Horton <2733550h@student.gla.ac.uk>
2025-02-19 15:09:45 +0000

    added frontend linting to pipeline

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,6 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -24,5 +23,6 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
+    ignores: ['dist', '.node_modules/*'],
+  }
 )
