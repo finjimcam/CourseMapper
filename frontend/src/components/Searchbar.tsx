8e0209b5686553cf00119c55806b9b51cbab15db
Rachel Horton <2733550h@student.gla.ac.uk>
2025-02-19 11:29:48 +0000

    Fixed placeholder message

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -21,7 +21,7 @@ function SearchBar() {
                     type="text"
                     id="simple-search"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    placeholder="Search workbook..."
+                    placeholder="Search workbooks..."
                     required
                     onChange={(e) => setSearchInput(e.target.value)}
                 />
