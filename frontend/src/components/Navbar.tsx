b175f994ecbcd19996394a1d42d8fd29e21d7f51
Maxine <maxine.armstrong2005@gmail.com>
2025-02-26 09:08:05 +0000

    done

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,21 @@
+/*
+Source code for LISU CCM @UofG
+Copyright (C) 2025 Maxine Armstrong, Ibrahim Asghar, Finlay Cameron, Colin Nardo, Rachel Horton, Qikai Zhou
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program at /LICENSE.md. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { useEffect, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { Breadcrumb, Dropdown } from 'flowbite-react';
