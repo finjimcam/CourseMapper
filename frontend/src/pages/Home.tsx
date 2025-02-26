b9a2afb96a03329752823b33cbb7fb714463d46e
Maxine <maxine.armstrong2005@gmail.com>
2025-02-26 09:42:30 +0000

    lint

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -22,14 +22,15 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import SearchBar from '../components/Searchbar.tsx';
 import Carousel from '../components/Carousel.tsx';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { CreateWorkbookModal } from '../components/modals/CreateWorkbookModal.tsx';
+import { getErrorMessage } from '../utils/workbookUtils.tsx';
 
 function Home() {
   const navigate = useNavigate();
   const [workbooks, setWorkbooks] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState('');
   const [showCreateModal, setShowCreateModal] = useState(false);
 
   useEffect(() => {
@@ -38,8 +39,8 @@ function Home() {
         const response = await axios.get(`${import.meta.env.VITE_API}/workbooks/`);
         setWorkbooks(response.data);
         setLoading(false);
-      } catch (err: any) {
-        setError(err.message || 'An error occurred');
+      } catch (err: unknown) {
+        setError(getErrorMessage(err));
         setLoading(false);
       }
     };
@@ -63,47 +64,47 @@ function Home() {
 
   return (
     <>
-    <div className="flex">
-    <div className="p-8 space-y-8">
-      <div className="flex flex-col items-centre space-y-4">
-        <div className="space-y-2">
-            <h2 className="text-3xl font-bold text-left">Welcome back, Tim!</h2>
-            <h3 className="text-lg text-left text-gray-600">
-              Explore and manage your courses with ease
-            </h3>
-        </div>
+      <div className="flex">
+        <div className="p-8 space-y-8">
+          <div className="flex flex-col items-centre space-y-4">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold text-left">Welcome back, Tim!</h2>
+              <h3 className="text-lg text-left text-gray-600">
+                Explore and manage your courses with ease
+              </h3>
+            </div>
 
-          <SearchBar />
-          <div className="flex gap-4">
-            <button
-              type="button"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition delay-150 duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
-              onClick={() => navigate('/my-workbooks')}>
+            <SearchBar />
+            <div className="flex gap-4">
+              <button
+                type="button"
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition delay-150 duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
+                onClick={() => navigate('/my-workbooks')}
+              >
                 My Workbooks
-            </button>
+              </button>
 
-            <button
-              type="button"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition delay-150 duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
-              onClick={() => setShowCreateModal(true)}>
+              <button
+                type="button"
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition delay-150 duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
+                onClick={() => setShowCreateModal(true)}
+              >
                 Create Workbook
-            </button>
-        </div>
+              </button>
+            </div>
 
-        <CreateWorkbookModal
-          show={showCreateModal}
-          onClose={() => setShowCreateModal(false)}
-          onSubmit={handleCreateWorkbook}
-        />
-        
-      </div>
-    </div>
-    <div className="flex flex-col justify-center items-center w-1/2 min-w-[400px]">
+            <CreateWorkbookModal
+              show={showCreateModal}
+              onClose={() => setShowCreateModal(false)}
+              onSubmit={handleCreateWorkbook}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center w-1/2 min-w-[400px]">
           <h1 className="text-2xl font-semibold text-left">Recent Workbooks</h1>
           <Carousel items={workbooks} />
         </div>
-
-        </div>
+      </div>
     </>
   );
 }
--- frontend/src/pages/MyWorkbooks.tsx
+++ frontend/src/pages/MyWorkbooks.tsx
@@ -68,40 +68,39 @@ function MyWorkbooks() {
 
   return (
     <>
-    <div className="flex">
-    <div className="p-8 space-y-8">
-      <div className="flex flex-col items-centre space-y-4">
-        <div className="space-y-2">
-            <h2 className="text-3xl font-bold text-left">Welcome back, Tim!</h2>
-            <h3 className="text-lg text-left text-gray-600">
-              Explore and manage your courses with ease
-            </h3>
-        </div>
+      <div className="flex">
+        <div className="p-8 space-y-8">
+          <div className="flex flex-col items-centre space-y-4">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold text-left">Welcome back, Tim!</h2>
+              <h3 className="text-lg text-left text-gray-600">
+                Explore and manage your courses with ease
+              </h3>
+            </div>
 
-          <SearchBar />
-          <div className="flex gap-4">
-            <button
-              type="button"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition delay-150 duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
-              onClick={() => setShowCreateModal(true)}>
+            <SearchBar />
+            <div className="flex gap-4">
+              <button
+                type="button"
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition delay-150 duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
+                onClick={() => setShowCreateModal(true)}
+              >
                 Create Workbook
-            </button>
-        </div>
+              </button>
+            </div>
 
-        <CreateWorkbookModal
-          show={showCreateModal}
-          onClose={() => setShowCreateModal(false)}
-          onSubmit={handleCreateWorkbook}
-        />
-        
-      </div>
-    </div>
-    <div className="flex flex-col justify-center items-center w-1/2 min-w-[400px]">
+            <CreateWorkbookModal
+              show={showCreateModal}
+              onClose={() => setShowCreateModal(false)}
+              onSubmit={handleCreateWorkbook}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center w-1/2 min-w-[400px]">
           <h1 className="text-2xl font-semibold text-left">Recent Workbooks</h1>
           <Carousel items={workbooks} />
         </div>
-
-        </div>
+      </div>
     </>
   );
 }
